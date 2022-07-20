4dbefd221356b72dc4efceac9464aaff1b2931e5
aAlternant <zurto.art@gmail.com>
2022-07-20 20:33:16 +0300

    fix comments count

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,7 +61,7 @@ export const Home = () => {
                 }}
                 createdAt={obj.createdAt}
                 viewsCount={obj.viewsCount}
-                commentsCount={3}
+                commentsCount={obj.comments.length}
                 tags={obj.tags}
                 isEditable={userData?._id === obj.user._id}
               />
